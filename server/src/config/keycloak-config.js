c0beea2a620f3859ed317b97611c4bddd2199397
Alexandre Junqueira <alexandre.junqueira@konkerlabs.com>
2021-04-16 10:42:56 -0300

    keycloak configuration by command line

--- server/src/config/keycloak-config.js
+++ server/src/config/keycloak-config.js
@@ -7,11 +7,11 @@ const chalk = require('chalk');
 let keycloak;
 
 var keycloakConfig = {
-  'realm': 'MyBusway',
-  'auth-server-url': 'http://localhost:5400/auth/',
+  'realm': process.env.KC_REALM || 'MyBusway',
+  'auth-server-url': process.env.KC_SERVER_URL || 'http://localhost:5400/auth/',
   'ssl-required': 'none',
-  'resource': 'mybusway-microservice',
-  'realmPublicKey': 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqm0Y/wQesVOcA0tAOETbVbuoOK3gTiZ7Nr9JS5PHJT8n7o93XHjXB8+ThsHmhKz/eoyp/9A4qPathRauzZI9vn/xHLnjHbtSH3abIRT4X3TuBJcZH+jOIVrH1f69MxdWoPW/sQrVad08DuNRhVon8gMW0p2ZoTV3xGgHCg2Efeux1e4ygatDGHvzWl6BaZeMx6dn/QACuDhys0O1QJGVJYKXSetifu8ocRpYDWUz0OPNiE6xWoilHJu+UhtlVldHVIIrBknyEVZmfQxxPBsz3OHLu1biv/WURDAtPWXYXrJ6XGDnie+2mDUPIXReWPqt2HQnhe246OW0HClAjim+MwIDAQAB',
+  'resource': process.env.KC_RESOURCE || 'mybusway-microservice',
+  'realmPublicKey': process.env.KC_REALM_PUBLIC_KEY,
   'bearer-only': true
 };
 
